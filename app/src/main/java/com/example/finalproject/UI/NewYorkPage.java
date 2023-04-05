69c1017725b643336e12812c73c8ff0bbfb7d7bc
AbdullahSabbagh <sabb0018@algonquinlive.com>
2023-04-04 21:59:27 -0400

    version1.0

---
+++ app/src/main/java/com/example/finalproject/UI/NewYorkPage.java
@@ -0,0 +1,16 @@
+package com.example.finalproject.UI;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.finalproject.R;
+
+public class NewYorkPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_york_page);
+    }
+}
